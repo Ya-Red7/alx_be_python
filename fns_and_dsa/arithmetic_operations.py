7f719f6fea9f86c8477ccd965a5d4574f02f6024
Ya-Red7 <149231087+Ya-Red7@users.noreply.github.com>
2024-09-27 11:47:16 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             return num1*num2
         case 'divide':
-            if num1==0:
+            if num2==0:
                 return "Undefined"
             return num1/num2
 
