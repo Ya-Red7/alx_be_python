601778e900550a33facc4bc5461cbd79b330f4ac
Ya-Red7 <149231087+Ya-Red7@users.noreply.github.com>
2024-09-27 11:55:49 +0300

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,8 +4,8 @@ current_date = datetime.now()
 Y,m,d,H,M,S = current_date.year,current_date.month,current_date.day,current_date.hour,current_date.minute,current_date.second
 def display_current_datetime():
 
-    print(f"Current date and time: {Y}-{m}-{d} {H}:{M}:{S}")
-display_current_datetime()
+    return "{Y}-{m}-{d} {H}:{M}:{S}"
+print(f"Current date and time: {display_current_datetime()}")
 def calculate_future_date(number_of_days):
     global current_date
     future_date = current_date + timedelta(days=number_of_days)
