b5f85fdf42f9be62d3d071446b6ec3f62c96350b
Yared W <yaredwondatir77@gmail.com>
2024-10-05 15:53:29 +0300

    bug fixed-2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self,account_balance=0):
+    def __init__(self,account_balance=0.00):
         self.account_balance = account_balance
         
     def deposit(self,amount):
@@ -10,5 +10,5 @@ class BankAccount:
         self.account_balance-=amount
         return 1
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance}")
 
